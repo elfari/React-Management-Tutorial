cb5264269df927e7fe2f0e99bef81af42328a8ac
username <user@email.mail>
2021-05-24 16:20:10 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+    'id' : 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : '이순신',
+    'birthday' : '900101',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name' : '김유신',
+    'birthday' : '800523',
+    'gender' : '남자',
+    'job' : '직장인'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birth={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
